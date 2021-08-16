f065b27f5c53c0d20563eee1f62801f310a823ff
Nahuel Clauser <nahuelclauser@gmail.com>
2021-08-16 13:43:20 -0300

    Weather App Grid Layout

--- src/components/CityList/CityList.Test.jsx
+++ src/components/CityList/CityList.Test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import CityList from './CityList'
 
 const cities = [
@@ -18,3 +18,16 @@ test("CityList render",async () => {
     expect(items).toHaveLength(4)
 
 })
+ 
+test("CityList click on item", async () => {
+    const fnClickOnItem = jest.fn()
+
+    const { findAllByRole } = render(<CityList cities={cities} onClickCity={fnClickOnItem} />)
+
+    const items = await findAllByRole("listitem")
+
+    fireEvent.click(items[0])
+
+    expect(fnClickOnItem).toHaveBeenCalledTimes(1)
+})
+
--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,31 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Grid } from '@material-ui/core'
 import CityInfo from './../CityInfo/CityInfo'
 import Weather from '../Weather/Weather'
 
-const renderCityAndCountry = cityAndCountry =>{
+const renderCityAndCountry = eventOnClickCity => cityAndCountry =>{
     const { city, country} = cityAndCountry
 
     return(
-        <li key = {city}>
-           <CityInfo city = {city} country = {country}/> 
-           <Weather temperature={10} state="sunny" />
+        <li key = {city} onClick={eventOnClickCity}>
+            <Grid container
+                justify="center"
+                allignItems ="center">
+                    <Grid item
+                        md={8}
+                        xs={12}>
+                        <CityInfo city = {city} country = {country}/>
+                    </Grid>
+                    <Grid item
+                        md={4}
+                        xs={12}>
+                        <Weather temperature={10} state="sunny" />
+                    </Grid>
+
+            </Grid>
         </li>
     ) 
 }
 
-const CityList = ({cities}) => {
+const CityList = ({cities, onClickCity}) => {
     return (
         <ul>
             {
-                cities.map(cityAndCountry => renderCityAndCountry(cityAndCountry))
+                cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry))
             }
         </ul>
     )
 }
 
 CityList.propTypes = {
-    cities: PropTypes.array.isRequired
+    cities: PropTypes.array.isRequired,
+    onClickCity: PropTypes.func.isRequired,
 }
 
 export default CityList
