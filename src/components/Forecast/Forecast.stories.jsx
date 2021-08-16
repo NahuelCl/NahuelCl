46ce8d4a63bb953bd3b72082293192796d6a6c56
Nahuel Clauser <nahuelclauser@gmail.com>
2021-08-16 17:23:12 -0300

    ForecastItem Stories

--- src/components/Forecast/Forecast.stories.jsx
+++ src/components/Forecast/Forecast.stories.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import Forecast from './Forecast'
 
 export default {
-    title: "Forecast",
+    title: 'Forecast',
     component: Forecast
 }
 
-const forecastItemList = []
-
-export const ForecastExample = () => (<Forecast forecastItemList={forecastItemList}/>)
+const forecastItemList = [
+    { hour: 18, state:"sunny", temperature:24, weekDay:"Jueves" },
+    { hour: 7, state:"cloud", temperature:22, weekDay:"Viernes"},
+    { hour: 5, state:"rain", temperature:5, weekDay:"Miercoles" },
+    { hour: 9, state:"fog", temperature:11, weekDay:"Sábado"},
+]
+ 
+export const ForecastExample = () => (<Forecast forecastItemList={forecastItemList} />)
