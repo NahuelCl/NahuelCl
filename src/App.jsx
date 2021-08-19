cb9e5697e54d884089995212e36b505aaa7ea9c5
Nahuel Clauser <nahuelclauser@gmail.com>
2021-08-19 18:21:29 -0300

    Jugando con router

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+
+const App = () => {
+    return (
+        <div>
+           <h1>App</h1> 
+            <Router>
+               <Switch>
+                   <Route exact path="/">
+                        Welcome
+                        <div>
+                        <Link to="/main">Ir a main</Link>
+                        </div>
+                   </Route>
+                   <Route path= "/main">
+                        Main
+
+                   </Route>
+                   <Route path="/city">
+                       City
+                       <div>
+                        <Link to="/main">Volver a main</Link>
+                        </div>
+                   </Route>
+                   <Route>
+                       Not Found
+                       <div>
+                        <Link to="/main">Volver a main</Link>
+                        </div>
+                   </Route>
+               </Switch>
+            </Router>
+        </div>
+    )
+}
+
+export default App
+    
